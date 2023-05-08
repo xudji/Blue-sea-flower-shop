8897cde5ce488c89e273e086ec8090e3681346be
Ji Xudong <jixudong99@163.com>
2023-05-08 23:12:06 +0800

    Render navicon

--- api/api.js
+++ api/api.js
@@ -4,4 +4,11 @@ export const findBannerApi = () => {
   return request({
    url:'/mall-api/index/findBanner'})
 }
+// navicon
+export const findCategory1Api = () => {
+  return request({
+   url:'/mall-api/index/findCategory1'})
+}
+
+// 商品数据
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,29 @@
 // index.js
 // 获取应用实例
-import {findBannerApi} from '../../api/api'
+import {findBannerApi,findCategory1Api} from '../../api/api'
 
 const app = getApp()
 
 Page({
   data: {
-    bannerList:[]
+    bannerList:[],
+    navList:[]
   },
  
   onLoad() {
-    this.getFindBanner()  
+    this.getFindBanner()
+    this.getFindCategory1() 
   },
   async getFindBanner(){
    const res = await findBannerApi()
      this.setData({
        bannerList: res.data
      })
+  },
+  async getFindCategory1(){
+   const res = await findCategory1Api()
+   this.setData({
+    navList:res.data
+   })
   }
 })
